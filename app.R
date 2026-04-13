6dbfb5268f831f7f90fb5e922c64695e8830b21c
Marcela Cardenas <mcards@liverpool.ac.uk>
2026-04-13 14:17:27 +0100

    With Plot

--- app.R
+++ app.R
@@ -1,16 +1,60 @@
+# rsconnect::writeManifest()
 library(shiny)
 library(tidyverse)
 library(DT)
+library('igraph')
+library(networkD3)
 
 # allLOs <- readRDS("./data/allLOs.rds")
 # processed <- readRDS("./data/processed.rds")
 # ASLOs <- readRDS("./data/ASLOs.rds")
 LOs <- readRDS("./data/LOs.rds")
 
+ColourScale <- 'd3.scaleOrdinal()
+            .domain(["LOID", "Source", "LO_ID"])
+           .range(["#FF6900", "#498c55","#694489"]);'
+
 system_options <- unique(LOs$System)
 cohorts <- unique(LOs$Cohort)
 sessns <- unique(LOs$`Session Name`)
 src <- levels(LOs$src)
+
+## functions ##
+forPlot <- function(df){
+  tmp1 <- df %>%
+    select(LOID, LO_ID)
+  tmp2 <- df %>%
+    select(src, LOID)
+  names(tmp1) <- c("Parent", "Child")
+  names(tmp2) <- names(tmp1)
+  data <- rbind(tmp1, tmp2)
+  return(data)
+}
+
+getGroups <- function(df){
+  tmp4 <- df %>%
+    distinct(LOID) %>%
+    mutate(grps = "LOID")
+  tmp5 <- df %>%
+    distinct(src) %>%
+    mutate(grps = "Source")
+  tmp6 <- df %>%
+    distinct(LO_ID) %>%
+    mutate(grps = "LO_ID")
+  names(tmp4)[1] <- "ID"
+  names(tmp5)[1] <- "ID"
+  names(tmp6)[1] <- "ID"
+  
+  grps <- rbind(tmp4,tmp5,tmp6)
+  rm(tmp4,tmp5,tmp6)
+  
+  grps <-grps %>%
+    distinct(ID, .keep_all = T)
+  
+  g <- as.character(grps$grps)
+  names(g) <- grps$ID
+  return(g)
+}
 
 ui <- fluidPage(
   titlePanel("HARC LOs mapping project"),
@@ -21,7 +65,7 @@ ui <- fluidPage(
                             selectInput("sys", "System/Region", choices = system_options, multiple = FALSE),
                             selectInput("coh", "Cohort", choices = cohorts, multiple = FALSE)
                ),
-               mainPanel(
+               mainPanel(width = 10,
                  fluidRow(
                    column(6,
                           h4("Select LOs to see new mapped LOs"),
@@ -43,7 +87,7 @@ ui <- fluidPage(
                             selectInput("sys2", "System/Region", choices = system_options, multiple = FALSE)
                ),
                mainPanel(width = 10,
-                         fluidRow(width = 12,
+                         fluidRow(
                                   column(6,
                                          h4("Select new LOs to see HARC mapped LOs"),
                                          dataTableOutput("tableAS")
@@ -56,7 +100,15 @@ ui <- fluidPage(
                )
              )
     ),
-    tabPanel("Plot")
+    tabPanel("Plot",
+             sidebarLayout(
+               sidebarPanel(width = 2,
+                            selectInput("src2", "Syllabus", choices = src, selected = "AS-Medicine"),
+                            h5("Orange: New LOs"),
+                            h5("Purple: Original LOs")),
+               mainPanel(forceNetworkOutput("force"))
+             )
+             )
   )
 )
 
@@ -138,7 +190,22 @@ server <- function(input, output, session) {
       select(LO_ID, LO_Description_OG)
   })
   
+  #### Tab 3: plot #######
+  plotLOs <- reactive(LOs %>% filter(src == input$src2))
   
+  plt_d3 <- reactive({
+    input$src2
+    igraph_to_networkD3(graph_from_data_frame(forPlot(plotLOs()), directed = T), group = getGroups(plotLOs()))
+    
+  })
+  
+  output$force <- renderForceNetwork({
+    req(input$src2)
+    forceNetwork(Links = plt_d3()$links, Nodes = plt_d3()$nodes,
+                 Source = 'source', Target = 'target', 
+                 NodeID = 'name', Group = 'group', zoom = T, opacity = 0.8, colourScale = ColourScale)
+  })
+
 }
 
 shinyApp(ui, server)
